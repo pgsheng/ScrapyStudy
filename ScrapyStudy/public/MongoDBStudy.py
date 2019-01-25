13673c8be7641df89d881005a8ff75e1735774d5
pgsheng <pgsheng09@163.com>
2019-01-25 10:28:36 +0800

    优化

--- ScrapyStudy/public/MongoDBStudy.py
+++ ScrapyStudy/public/MongoDBStudy.py
@@ -16,8 +16,7 @@ class MongoDBStudy(object):
             连接MongoDB我们需要使用PyMongo库里面的MongoClient，一般来说传入MongoDB的IP及端口即可，第一个参数为地址host，
             第二个参数为端口port，端口如果不传默认是27017。
         """
-        self.client = pymongo.MongoClient(host='localhost', port=27017)
-        # client = pymongo.MongoClient('mongodb://localhost:27017/')  # 可以达到同样的连接效果。
+        self.client = pymongo.MongoClient('mongodb://admin:pgsheng123@localhost:27017/')  # 可以达到同样的连接效果。
         """
         2、指定数据库
             MongoDB分为一个个数据库，需要指定要操作哪个数据库，在这里我以test数据库为例进行说明
